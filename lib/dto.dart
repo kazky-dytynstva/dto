2ba128f5f33f470fb281bc00e487970668cdde72
Andrii Antonov <pro100svitlo@gmail.com>
2025-05-20 20:53:28 +0200

    fix: correct export paths for audio_content and text_content DTOs

--- lib/dto.dart
+++ lib/dto.dart
@@ -7,8 +7,8 @@ export 'src/person/person_dto.dart';
 export 'src/person/person_gender_dto.dart';
 export 'src/person/person_role_dto.dart';
 
-export 'src/tale/content/audio_content.dart';
-export 'src/tale/content/text_content.dart';
+export 'src/tale/content/audio_content_dto.dart';
+export 'src/tale/content/text_content_dto.dart';
 export 'src/tale/crew/crew_dto.dart';
 export 'src/tale/rating/rating_dto.dart';
 export 'src/tale/tale_dto.dart';
