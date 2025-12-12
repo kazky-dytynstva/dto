e87298587fe3e1247febe28e174661062b25d929
Andrii Antonov <pro100svitlo@gmail.com>
2025-12-12 10:14:31 +0100

    feat: refactor DTOCompression to support generics for compressing and decompressing DTOs

--- lib/src/compression/dto_compression.dart
+++ lib/src/compression/dto_compression.dart
@@ -5,7 +5,54 @@ import 'package:dto/src/person/person_dto.dart';
 import 'package:dto/src/tale/tale_dto.dart';
 
 /// A utility class for compressing and decompressing DTOs using gzip compression.
-class DTOCompression {
+class DTOCompression<T> {
+  /// Creates a DTO compression instance.
+  const DTOCompression();
+
+  /// Compresses a list of objects into a gzip-compressed byte array.
+  ///
+  /// The items are first serialized to JSON using [toJson], then encoded to UTF-8,
+  /// and finally compressed using gzip compression.
+  ///
+  /// [items] - The list of objects to compress.
+  /// [toJson] - Function to convert each item to a JSON map.
+  ///
+  /// Returns a [Uint8List] containing the compressed data.
+  Uint8List compress(List<T> items, Map<String, dynamic> Function(T) toJson) {
+    final jsonString = jsonEncode(items.map(toJson).toList());
+    final bytes = utf8.encode(jsonString);
+    final compressed = gzip.encode(bytes);
+    return compressed is Uint8List
+        ? compressed
+        : Uint8List.fromList(compressed);
+  }
+
+  /// Decompresses a gzip-compressed byte array into a list of objects.
+  ///
+  /// The compressed data is first decompressed using gzip, then decoded from UTF-8,
+  /// parsed as JSON, and finally converted to objects using [fromJson].
+  ///
+  /// [compressedData] - The compressed byte array.
+  /// [fromJson] - Function to convert each JSON map to an object of type [T].
+  ///
+  /// Returns a [List<T>] containing the decompressed objects.
+  ///
+  /// Throws [FormatException] if the compressed data is invalid or cannot be parsed.
+  List<T> decompress(
+    Uint8List compressedData,
+    T Function(Map<String, dynamic>) fromJson,
+  ) {
+    final decompressedBytes = gzip.decode(compressedData);
+    final jsonString = utf8.decode(decompressedBytes);
+    final jsonList = jsonDecode(jsonString) as List<dynamic>;
+    return jsonList
+        .map((json) => fromJson(json as Map<String, dynamic>))
+        .toList();
+  }
+
+  static final _taleCompression = DTOCompression<TaleDto>();
+  static final _personCompression = DTOCompression<PersonDto>();
+
   /// Compresses a list of [TaleDto] objects into a gzip-compressed byte array.
   ///
   /// The tales are first serialized to JSON, then encoded to UTF-8, and finally
@@ -13,7 +60,7 @@ class DTOCompression {
   ///
   /// Returns a [Uint8List] containing the compressed data.
   static Uint8List compressTales(List<TaleDto> tales) {
-    return _compress(tales, (tale) => tale.toJson());
+    return _taleCompression.compress(tales, (tale) => tale.toJson());
   }
 
   /// Decompresses a gzip-compressed byte array into a list of [TaleDto] objects.
@@ -25,7 +72,7 @@ class DTOCompression {
   ///
   /// Throws [FormatException] if the compressed data is invalid or cannot be parsed.
   static List<TaleDto> decompressTales(Uint8List compressedData) {
-    return _decompress(compressedData, (json) => TaleDto.fromJson(json));
+    return _taleCompression.decompress(compressedData, TaleDto.fromJson);
   }
 
   /// Compresses a list of [PersonDto] objects into a gzip-compressed byte array.
@@ -35,7 +82,7 @@ class DTOCompression {
   ///
   /// Returns a [Uint8List] containing the compressed data.
   static Uint8List compressPeople(List<PersonDto> people) {
-    return _compress(people, (person) => person.toJson());
+    return _personCompression.compress(people, (person) => person.toJson());
   }
 
   /// Decompresses a gzip-compressed byte array into a list of [PersonDto] objects.
@@ -47,32 +94,6 @@ class DTOCompression {
   ///
   /// Throws [FormatException] if the compressed data is invalid or cannot be parsed.
   static List<PersonDto> decompressPeople(Uint8List compressedData) {
-    return _decompress(compressedData, (json) => PersonDto.fromJson(json));
-  }
-
-  /// Generic compression method to avoid code duplication.
-  static Uint8List _compress<T>(
-    List<T> items,
-    Map<String, dynamic> Function(T) toJson,
-  ) {
-    final jsonString = jsonEncode(items.map(toJson).toList());
-    final bytes = utf8.encode(jsonString);
-    final compressed = gzip.encode(bytes);
-    return compressed is Uint8List
-        ? compressed
-        : Uint8List.fromList(compressed);
-  }
-
-  /// Generic decompression method to avoid code duplication.
-  static List<T> _decompress<T>(
-    Uint8List compressedData,
-    T Function(Map<String, dynamic>) fromJson,
-  ) {
-    final decompressedBytes = gzip.decode(compressedData);
-    final jsonString = utf8.decode(decompressedBytes);
-    final jsonList = jsonDecode(jsonString) as List<dynamic>;
-    return jsonList
-        .map((json) => fromJson(json as Map<String, dynamic>))
-        .toList();
+    return _personCompression.decompress(compressedData, PersonDto.fromJson);
   }
 }
